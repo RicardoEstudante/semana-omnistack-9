899c995744e814d1d61bc3384ef3f941c9a31cf6
RicardoEstudante <ricardo2010carvalho@hotmail.com>
2020-10-07 09:01:56 -0500

    add nodemon

--- src/server.js
+++ src/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.json({ Message: 'Hello Omnistack' });
+});
+
 app.listen(3333);
